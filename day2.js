3ff87de6a41305029953a014a1a6726decc2d2f9
avinashkumarsingh2525-a11y <avinashkumarsingh2525@gmail.com>
2026-09-07 12:13:03 +0530

    Add initial code for Day 2 exercises

---
+++ day2.js
@@ -0,0 +1,9 @@
+// Day 2: Data Types & Operators
+let name = "Avinash";
+let age = 21;
+let isLearning = true;
+console.log("Name:", name);
+console.log("Type:", typeof age);
+let a = 10; let b = 3;
+console.log("Add:", a+b);
+console.log("Next year age:", age+1);
