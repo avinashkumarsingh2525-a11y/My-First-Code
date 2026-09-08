cd64ed4187aaa478fd5ec4c49633f0a53305984b
avinashkumarsingh2525-a11y <avinashkumarsingh2525@gmail.com>
2026-09-08 12:24:01 +0530

    Create day3.js

---
+++ day3.js
@@ -0,0 +1,17 @@
+// Day 3: If-Else
+
+let marks = 75;
+
+if (marks >= 33) {
+  console.log("Pass ho gaye! Marks:", marks);
+} else {
+  console.log("Fail ho gaye! Marks:", marks);
+}
+
+let age = 17;
+
+if (age >= 18) {
+  console.log("Vote de sakte ho");
+} else {
+  console.log("Abhi nahi, chhote ho");
+}
